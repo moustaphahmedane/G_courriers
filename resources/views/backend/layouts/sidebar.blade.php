004b19747248dc972d0ed65e7f6ed520cdef96b9
moustaphahmedane <mohamedelmoustaphath@gmail.com>
2023-08-14 13:04:35 +0000

    commit initial

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -17,7 +17,6 @@
 
       <!-- SidebarSearch Form -->
 
-
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
